a767cbd29d0a9e704e15b4f48bbad98ab674e252
Essam Hassan <ic15b074@technikum-wien.at>
2016-02-09 23:27:34 +0100

    add project Task1 1.6

---
+++ taks/Task1/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var x = "Hello world";
+            var a = "!";
+            Console.WriteLine("{0} {1} {0}", a , x);
+        }
+    }
+}
