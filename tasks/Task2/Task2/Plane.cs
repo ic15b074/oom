e21f759657ca83a1763249d825f8943bb76a8943
Essam HASSAN <ic15b074@technikum-wien.at>
2016-02-21 13:01:27 +0100

    add projekt Task2

---
+++ tasks/Task2/Task2/Plane.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Plane
+    {
+
+        private int maxFuel;
+
+        public Plane(string planeNumber, int maxFuel){
+
+            this.PlaneNummber = planeNumber;
+            this.maxFuel = maxFuel;
+        }
+
+        
+        public string PlaneNummber { get;}
+        public int NummberOfSeats { get; set; }
+
+        public int ShowMaxFuelInfo()
+        {
+            return maxFuel;
+        }
+        
+    }
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var planes = new List<Plane>();
+
+            var plane1 = new Plane("3HF543", 250);
+            var plane2 = new Plane("3GJ234", 200);
+            var plane3 = new Plane("3SD654", 350);
+            var plane4 = new Plane("3KJ897", 150);
+
+            planes.Add(plane1);
+            planes.Add(plane2);
+            planes.Add(plane3);
+            planes.Add(plane4);
+
+            foreach(var plane in planes)
+            {
+                Console.WriteLine("Number of Seats:{0} Plane Number: {1}",plane.NummberOfSeats, plane.PlaneNummber);
+                Console.WriteLine("Max Fuel Info:{0}", plane.ShowMaxFuelInfo());
+            }
+
+            plane2.NummberOfSeats = 10;
+
+            Console.WriteLine();
+
+            foreach (var plane in planes)
+            {
+                Console.WriteLine("Number of Seats:{0} Plane Number: {1}", plane.NummberOfSeats, plane.PlaneNummber);
+                Console.WriteLine("Max Fuel Info:{0}", plane.ShowMaxFuelInfo());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
